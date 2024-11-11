f1fd18bc0fd1cf353dc815631b9b731c374f4b9d
Bicho1302 <victorsn13022003@gmail.com>
2024-11-10 22:26:26 -0700

    week 01

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+
+const currentYear = new Date().getFullYear(); 
+document.getElementById('2024').textContent = currentYear; 
+
+
+const lastModifiedDate = document.lastModified;  
+document.getElementById('lastModified').textContent = "Last Modified: " + lastModifiedDate; 
